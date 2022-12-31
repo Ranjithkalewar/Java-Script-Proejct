7e10bf4d2960e01aa6ef0b8923aa1a9ed16e494c
kalewarranjith <Kalewars99>
2022-12-31 15:59:44 +0100

    date

---
+++ class 4/date.js
@@ -0,0 +1,8 @@
+
+let d=new Date();
+document.write("Date :"+d.getDate()+"<br>");
+document.write("Date :"+d.getDay()+"<br>");
+document.write("Date :"+d.getHours()+"<br>");
+document.write("Date :"+d.getMinutes()+"<br>");
+document.write("Date :"+d.getSeconds()+"<br>");
+document.write("Date :"+d.getMilliseconds()+"<br>");
